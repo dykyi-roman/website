90829deebd96012ea71e61186488fac0f735a58b
dykyi.roman <mr.dukuy@gmail.com>
2024-12-03 13:37:09 +0400

    refactoring

--- code/config/bundles.php
+++ code/config/bundles.php
@@ -7,4 +7,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Chrisguitarguy\RequestId\ChrisguitarguyRequestIdBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
