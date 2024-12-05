7af971173de56213f0d87acd2063393693c9b3cc
dykyi.roman <mr.dukuy@gmail.com>
2024-12-05 08:53:09 +0400

    write tests

--- code/src/Service/Infrastructure/ElasticSearch/Client.php
+++ code/src/Service/Infrastructure/ElasticSearch/Client.php
@@ -12,7 +12,9 @@ final class Client implements ServiceInterface
     {
         return [
             [
+                'id' => 1,
                 'title' => 'Sample Service Title 1',
+                'category' => 'Phone',
                 'description' => 'This is a sample description for the first service offering. This is a sample description for the first service offering. This is a sample description for the first service offering. This is a sample description for the first service offering.',
                 'feedback_count' => '42',
                 'image_url' => 'https://dykyi-roman.github.io/images/photo.jpg',
@@ -28,6 +30,8 @@ final class Client implements ServiceInterface
                 'price' => '500'
             ],
             [
+                'id' => 2,
+                'category' => 'TV',
                 'title' => 'Sample Service Title 2',
                 'description' => 'This is a sample description for the second service offering.',
                 'feedback_count' => '38',
@@ -42,7 +46,43 @@ final class Client implements ServiceInterface
                 'rating' => 2,
                 'review_count' => 345,
                 'price' => '500'
-            ]
+            ],
+            [
+                'id' => 3,
+                'category' => 'TV',
+                'title' => 'Sample Service Title 2',
+                'description' => 'This is a sample description for the second service offering.',
+                'feedback_count' => '38',
+                'image_url' => 'https://dykyi-roman.github.io/images/photo.jpg',
+                'features' => [
+                    'Super-premium',
+                    'Master-freelancer',
+                    '98% positive reviews',
+                    'Online 2 hours ago',
+                    'Response time: 1 hour'
+                ],
+                'rating' => 2,
+                'review_count' => 345,
+                'price' => '500'
+            ],
+            [
+                'id' => 4,
+                'category' => 'TV',
+                'title' => 'Sample Service Title 2',
+                'description' => 'This is a sample description for the second service offering.',
+                'feedback_count' => '38',
+                'image_url' => 'https://dykyi-roman.github.io/images/photo.jpg',
+                'features' => [
+                    'Super-premium',
+                    'Master-freelancer',
+                    '98% positive reviews',
+                    'Online 2 hours ago',
+                    'Response time: 1 hour'
+                ],
+                'rating' => 2,
+                'review_count' => 345,
+                'price' => '500'
+            ],
         ];
     }
 }
