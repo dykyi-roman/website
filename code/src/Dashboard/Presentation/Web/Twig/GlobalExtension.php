40922b6def1b49ffd6b889f4b1fa6a5865d864b6
dykyi.roman <mr.dukuy@gmail.com>
2024-12-07 08:44:59 +0400

    development

--- code/src/Dashboard/Presentation/Web/Twig/GlobalExtension.php
+++ code/src/Dashboard/Presentation/Web/Twig/GlobalExtension.php
@@ -11,6 +11,7 @@ use Twig\Extension\GlobalsInterface;
 final class GlobalExtension extends AbstractExtension implements GlobalsInterface
 {
     public function __construct(
+        private readonly string $appName,
         private readonly Security $security,
     ) {
     }
@@ -32,7 +33,7 @@ final class GlobalExtension extends AbstractExtension implements GlobalsInterfac
 
         // Organization data
         $response['organization'] = [
-            'name' => 'EasyOrder',
+            'name' => $this->appName,
             'phone' => '+1-XXX-XXX-XXXX',
             'social_links' => [
                 'https://www.facebook.com/easy-order',
