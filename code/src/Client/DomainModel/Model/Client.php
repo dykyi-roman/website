bba8c26b48e6f4d4ca637fd1e39d3f5876b3b862
dykyi.roman <mr.dukuy@gmail.com>
2024-12-10 12:11:57 +0400

    add 2 db

---
+++ code/src/Client/DomainModel/Model/Client.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Client\DomainModel\Model;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class Client implements UserInterface
+{
+
+    public function getRoles(): array
+    {
+        // TODO: Implement getRoles() method.
+    }
+
+    public function eraseCredentials(): void
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getUserIdentifier(): string
+    {
+        // TODO: Implement getUserIdentifier() method.
+    }
+}
