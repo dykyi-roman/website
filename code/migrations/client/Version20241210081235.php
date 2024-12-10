506ea1901e78c6f0d3c772e4b41664aa88ec37f3
dykyi.roman <mr.dukuy@gmail.com>
2024-12-10 12:32:15 +0400

    add orm

--- code/migrations/client/Version20241210081235.php
+++ code/migrations/client/Version20241210081235.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace DoctrineMigrations\client;
+namespace App\Migration\Client;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
--- code/migrations/partner/Version20241210081236.php
+++ code/migrations/partner/Version20241210081236.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace DoctrineMigrations\partner;
+namespace App\Migration\Partner;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
