48076bc2c297ae7d12a3c749120cd0938a8f609b
dykyi.roman <mr.dukuy@gmail.com>
2024-12-10 12:58:13 +0400

    add orm

--- code/migrations/partner/Version20241210084422.php
+++ code/migrations/partner/Version20241210084422.php
@@ -7,7 +7,7 @@ namespace App\Migration\Partner;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20241210081236 extends AbstractMigration
+final class Version20241210084422 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -16,27 +16,24 @@ final class Version20241210081236 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        $this->addSql('DROP TABLE IF EXISTS partner');
+
         $this->addSql('CREATE TABLE partner (
-            id UUID NOT NULL,
+            id BINARY(16) NOT NULL,
             name VARCHAR(100) NOT NULL,
             email VARCHAR(64) NOT NULL,
             phone VARCHAR(20) DEFAULT NULL,
             country VARCHAR(100) DEFAULT NULL,
             city VARCHAR(255) DEFAULT NULL,
             status SMALLINT DEFAULT 1,
-            phone_verified_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL,
-            email_verified_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL,
-            activated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL,
-            deactivated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL,
-            created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-            updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+            phone_verified_at DATETIME DEFAULT NULL,
+            email_verified_at DATETIME DEFAULT NULL,
+            activated_at DATETIME DEFAULT NULL,
+            deactivated_at DATETIME DEFAULT NULL,
+            created_at DATETIME NOT NULL,
+            updated_at DATETIME NOT NULL,
             PRIMARY KEY(id)
-        )');
-        
-        $this->addSql('CREATE TRIGGER update_partner_updated_at
-            BEFORE UPDATE ON partner
-            FOR EACH ROW
-            EXECUTE FUNCTION update_updated_at_column();');
+        ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
