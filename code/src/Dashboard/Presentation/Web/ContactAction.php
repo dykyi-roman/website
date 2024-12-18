c6ea4ebdf61cbfd37f8a9dc637a90c2e14654400
dykyi.roman <mr.dukuy@gmail.com>
2024-12-18 18:50:59 +0400

    +

--- code/src/Dashboard/Presentation/Web/ContactAction.php
+++ code/src/Dashboard/Presentation/Web/ContactAction.php
@@ -16,8 +16,6 @@ final readonly class ContactAction
         ContactHtmlResponder $responder,
         TranslatorInterface $translator,
     ): ContactHtmlResponder {
-        throw new NotFoundHttpException('Oh nowwwww', 404);
-
         return $responder->respond([
             'page_title' => $translator->trans('contact.page_title'),
             'content' => $translator->trans('contact.content'),
