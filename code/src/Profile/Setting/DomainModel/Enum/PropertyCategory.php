ee263bc2f55b6a543136f8f2da221ecd4d0f9c6b
dykyi.roman <mr.dukuy@gmail.com>
2025-01-09 12:56:29 +0400

    fix ci;

--- code/src/Profile/Setting/DomainModel/Enum/PropertyCategory.php
+++ code/src/Profile/Setting/DomainModel/Enum/PropertyCategory.php
@@ -10,6 +10,7 @@ enum PropertyCategory: string
     case ACCOUNT = 'ACCOUNT';
     case NOTIFICATION = 'NOTIFICATION';
 
+    /** @return array<string> */
     public static function values(): array
     {
         return array_column(self::cases(), 'value');
--- code/src/Profile/Setting/DomainModel/Enum/PropertyName.php
+++ code/src/Profile/Setting/DomainModel/Enum/PropertyName.php
@@ -14,6 +14,7 @@ enum PropertyName: string
     case Setting_SETTINGS_GENERAL_CURRENCY = 'currency';
     case Setting_SETTINGS_GENERAL_THEME = 'theme';
 
+    /** @return array<string> */
     public static function values(): array
     {
         return array_column(self::cases(), 'value');
