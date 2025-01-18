15164dd966543f55307835c575920399e5712a86
dykyi.roman <mr.dukuy@gmail.com>
2025-01-18 09:20:33 +0400

    write notification;

--- code/src/Services/Infrastructure/ElasticSearch/MockClient.php
+++ code/src/Services/Infrastructure/ElasticSearch/MockClient.php
@@ -43,13 +43,13 @@ final readonly class MockClient implements ServicesInterface
                 'url' => '/services/item/'.$i,
                 'feedback_count' => '42',
                 'image_url' => '',
-                'features' => [
+                'features' => array_values([
                     'Super-premium',
                     'Master-freelancer',
                     '95% positive reviews',
                     'Online 4 hours ago',
                     'Response time: 2 hours',
-                ],
+                ]),
                 'rating' => 4,
                 'review_count' => 34,
                 'price' => 100.00,
