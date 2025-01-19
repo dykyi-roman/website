31567d6ea1db159e24ec7461be47bddc732ac2f5
dykyi.roman <mr.dukuy@gmail.com>
2025-01-19 17:36:29 +0400

    write notification;

--- code/src/Notifications/Presentation/Api/DeleteNotificationAction.php
+++ code/src/Notifications/Presentation/Api/DeleteNotificationAction.php
@@ -14,7 +14,7 @@ use Symfony\Component\Routing\Attribute\Route;
 final class DeleteNotificationAction
 {
     #[Route('/v1/notifications/{id}', methods: ['DELETE'])]
-    #[OA\Put(
+    #[OA\Delete(
         path: '/api/v1/notifications/{id}',
         description: 'Marks a specific notification as delete for the authenticated user',
         summary: 'Mark notification as delete',
