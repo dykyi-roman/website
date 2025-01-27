a523076e0b2afbbe7e1e50bd0c5342e44ed93d12
dykyi.roman <mr.dukuy@gmail.com>
2025-01-27 18:05:14 +0400

    statuses;

--- code/src/Profile/UserStatus/Presentation/Console/SynchronizeUserStatusCommand.php
+++ code/src/Profile/UserStatus/Presentation/Console/SynchronizeUserStatusCommand.php
@@ -7,21 +7,24 @@ namespace Profile\UserStatus\Presentation\Console;
 use Profile\UserStatus\Application\GetUserStatus\Service\UserStatusService;
 use Profile\UserStatus\Application\UpdateUserStatus\Command\UpdateUserStatusCommand;
 use Profile\UserStatus\DomainModel\Dto\UserUpdateStatus;
+use Profile\UserStatus\DomainModel\Model\UserStatus;
 use Profile\UserStatus\DomainModel\Repository\UserStatusRepositoryInterface;
 use Shared\DomainModel\Services\MessageBusInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
     name: 'app:profile:user-status:sync',
-    description: 'Create a migration for a specific domain'
+    description: 'Synchronize user online/offline status between Redis and database'
 )]
 final class SynchronizeUserStatusCommand extends Command
 {
     private const int BATCH_SIZE = 100;
+    private const int MIN_BATCH_SIZE = 1;
 
     public function __construct(
         private readonly UserStatusService $userStatusService,
@@ -45,59 +48,145 @@ final class SynchronizeUserStatusCommand extends Command
             );
     }
 
+    /**
+     * @throws \InvalidArgumentException When batch size option is invalid
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln('<info>Starting synchronization...</info>');
+        try {
+            $output->writeln('<info>Starting synchronization...</info>');
 
-        $onlineUsersFromRedis = $this->userStatusService->getAllUserStatuses();
-        $onlineUsersInDb = $this->userStatusRepository->findAllOnline();
+            $batchSize = $this->getValidatedBatchSize($input);
+            
+            // Synchronize offline users
+            $this->synchronizeOfflineUsers($output, $batchSize);
+            
+            // Synchronize online users
+            $this->synchronizeOnlineUsers($output, $batchSize);
 
-        $batchSize = $this->getIntOption($input, 'batch-size');
-
-        $items = [];
-        $onlineUserIdsFromRedis = array_column($onlineUsersFromRedis, 'userId');
-        $validBatchSize = max(1, min($batchSize, self::BATCH_SIZE));
-        foreach (array_chunk($onlineUsersInDb, $validBatchSize) as $batch) {
-            foreach ($batch as $userStatus) {
-                if (!in_array($userStatus->getUserId(), $onlineUserIdsFromRedis, true)) {
-                   $items[] = new UpdateUserStatusCommand([
-                        'user_id' => $userStatus->getUserId()->toRfc4122(),
-                        'is_online' => false,
-                        'last_online_at' => $userStatus->getLastOnlineAt()->format('c'),
-                    ]);
-                }
-            }
-
-            $this->messageBus->dispatch(...$items);
+            $output->writeln('<info>Synchronization completed successfully.</info>');
+            
+            return Command::SUCCESS;
+        } catch (\Throwable $e) {
+            $output->writeln(sprintf('<error>Error during synchronization: %s</error>', $e->getMessage()));
+            return Command::FAILURE;
         }
-
-        $items = [];
-        foreach (array_chunk($onlineUsersFromRedis, $validBatchSize) as $batch) {
-            /** @var UserUpdateStatus[] $batch */
-            foreach ($batch as $status) {
-                $items[] = new UpdateUserStatusCommand([
-                    'user_id' => $status->userId->toRfc4122(),
-                    'is_online' => $status->isOnline,
-                    'last_online_at' => $status->lastOnlineAt->format('c'),
-                ]);
-            }
-
-            $this->messageBus->dispatch(...$items);
-
-            $progress = round((count($batch) / count($onlineUsersFromRedis)) * 100, 2);
-            $output->writeln(sprintf('<info>Progress: %s%%</info>', $progress));
-        }
-
-        return Command::SUCCESS;
     }
 
-    private function getIntOption(InputInterface $input, string $name): int
+    private function synchronizeOfflineUsers(OutputInterface $output, int $batchSize): void
     {
-        $value = $input->getOption($name);
-        if (!is_int($value)) {
-            throw new \InvalidArgumentException(sprintf('%s must be a int', $name));
+        $onlineUsersFromRedis = $this->userStatusService->getAllUserStatuses();
+        $onlineUsersInDb = $this->userStatusRepository->findAllOnline();
+        $onlineUserIdsFromRedis = array_column($onlineUsersFromRedis, 'userId');
+
+        $output->writeln('<info>Processing offline users...</info>');
+        $progressBar = $this->createProgressBar($output, count($onlineUsersInDb));
+
+        foreach (array_chunk($onlineUsersInDb, $batchSize) as $batch) {
+            $commands = $this->createOfflineStatusCommands($batch, $onlineUserIdsFromRedis);
+            if (!empty($commands)) {
+                $this->dispatchCommands($commands);
+            }
+            $progressBar->advance(count($batch));
         }
 
-        return $value;
+        $progressBar->finish();
+        $output->writeln('');
+    }
+
+    private function synchronizeOnlineUsers(OutputInterface $output, int $batchSize): void
+    {
+        $onlineUsersFromRedis = $this->userStatusService->getAllUserStatuses();
+
+        $output->writeln('<info>Processing online users...</info>');
+        $progressBar = $this->createProgressBar($output, count($onlineUsersFromRedis));
+
+        foreach (array_chunk($onlineUsersFromRedis, $batchSize) as $batch) {
+            /** @var UserUpdateStatus[] $batch */
+            $commands = $this->createOnlineStatusCommands($batch);
+            if (!empty($commands)) {
+                $this->dispatchCommands($commands);
+            }
+            $progressBar->advance(count($batch));
+        }
+
+        $progressBar->finish();
+        $output->writeln('');
+    }
+
+    /**
+     * @param UserStatus[] $batch
+     * @param array<string|\Shared\DomainModel\ValueObject\UserId> $onlineUserIdsFromRedis
+     *
+     * @return UpdateUserStatusCommand[]
+     */
+    private function createOfflineStatusCommands(array $batch, array $onlineUserIdsFromRedis): array
+    {
+        $commands = [];
+        foreach ($batch as $userStatus) {
+            if (!in_array($userStatus->getUserId(), $onlineUserIdsFromRedis, true)) {
+                $commands[] = new UpdateUserStatusCommand([
+                    'user_id' => $userStatus->getUserId()->toRfc4122(),
+                    'is_online' => false,
+                    'last_online_at' => $userStatus->getLastOnlineAt()->format('c'),
+                ]);
+            }
+        }
+        return $commands;
+    }
+
+    /**
+     * @param UserUpdateStatus[] $batch
+     *
+     * @return UpdateUserStatusCommand[]
+     */
+    private function createOnlineStatusCommands(array $batch): array
+    {
+        return array_map(
+            static fn(UserUpdateStatus $status): UpdateUserStatusCommand => new UpdateUserStatusCommand([
+                'user_id' => $status->userId->toRfc4122(),
+                'is_online' => $status->isOnline,
+                'last_online_at' => $status->lastOnlineAt->format('c'),
+            ]),
+            $batch
+        );
+    }
+
+    /**
+     * @param UpdateUserStatusCommand[] $commands
+     *
+     * @throws \Throwable
+     */
+    private function dispatchCommands(array $commands): void
+    {
+        $this->messageBus->dispatch(...$commands);
+    }
+
+    private function createProgressBar(OutputInterface $output, int $max): ProgressBar
+    {
+        $progressBar = new ProgressBar($output, $max);
+        $progressBar->setFormat(' %current%/%max% [%bar%] %percent:3s%% %elapsed:6s%');
+
+        return $progressBar;
+    }
+
+    /**
+     * @throws \InvalidArgumentException When batch size is invalid
+     */
+    private function getValidatedBatchSize(InputInterface $input): int
+    {
+        $value = $input->getOption('batch-size');
+        if (!is_numeric($value)) {
+            throw new \InvalidArgumentException('Batch size must be a number');
+        }
+
+        $batchSize = (int) $value;
+        if ($batchSize < self::MIN_BATCH_SIZE) {
+            throw new \InvalidArgumentException(
+                sprintf('Batch size must be at least %d', self::MIN_BATCH_SIZE)
+            );
+        }
+
+        return min($batchSize, self::BATCH_SIZE);
     }
 }
